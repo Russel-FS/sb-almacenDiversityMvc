3fc8969ab01933b60317c62c31c9a7204447992b
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-14 01:21:53 -0500

    refactor: Agregar anotación @GeneratedValue en idProducto para mejorar la gestión automática de IDs en ProductoEntity

--- src/main/java/com/api/diversity/domain/model/ProductoEntity.java
+++ src/main/java/com/api/diversity/domain/model/ProductoEntity.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.Data;
 public class ProductoEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_Producto")
     private Long idProducto;
 
