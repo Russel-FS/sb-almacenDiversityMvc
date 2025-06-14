1eaeb723d68068fef72d84335ba1aa7bdf3863df
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-13 23:45:19 -0500

    feat: Integrate Cloudinary for file upload and management with configuration and service implementation

---
+++ src/main/java/com/api/diversity/application/service/impl/CloudinaryService.java
@@ -0,0 +1,34 @@
+package com.api.diversity.application.service.impl;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    
+    @Autowired
+    private Cloudinary cloudinary;
+    
+    public String uploadFile(MultipartFile file) {
+        try {
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), Map.of());
+            return uploadResult.get("url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Error uploading file to Cloudinary", e);
+        }
+    }
+    
+    public void deleteFile(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, Map.of());
+        } catch (IOException e) {
+            throw new RuntimeException("Error deleting file from Cloudinary", e);
+        }
+    }
+}
