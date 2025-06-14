e44ac62dd9a62fc6bb1aba88205030db39899126
bet <u00031041@sise.com.pe>
2025-06-14 03:15:24 -0500

    Update ClienteServicelmpl.java

--- src/main/java/com/api/diversity/application/service/impl/ClienteServicelmpl.java
+++ src/main/java/com/api/diversity/application/service/impl/ClienteServicelmpl.java
@@ -1,19 +1,15 @@
 package com.api.diversity.application.service.impl;
 
-
+import com.api.diversity.application.service.interfaces.ClienteService;
+import com.api.diversity.domain.model.Cliente;
+import com.api.diversity.infrastructure.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.api.diversity.application.service.interfaces.ClienteService;
-import com.api.diversity.domain.model.Cliente;
-import com.api.diversity.domain.model.Usuario;
-
-import com.api.diversity.infrastructure.repository.ClienteRepository;
-
-public class ClienteServicelmpl implements ClienteService {
+@Service
+public class ClienteServicelmpl implements ClienteService { 
 
     @Autowired
     private ClienteRepository clienteRepository;
@@ -29,19 +25,13 @@ public class ClienteServicelmpl implements ClienteService {
     }
 
     @Override
-    public Cliente getClienteById(Integer id) {
-        Optional<Cliente> optional = clienteRepository.findById(id);
-        Cliente cliente;
-        if (optional.isPresent()) {
-            cliente = optional.get();
-        } else {
-            throw new RuntimeException("Cliente no encontrado por id: " + id);
-        }
-        return cliente;
+    public Cliente getClienteById(Integer id) { 
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado por id: " + id));
     }
 
     @Override
-    public void deleteClienteById(Integer id) {
+    public void deleteClienteById(Integer id) { 
         this.clienteRepository.deleteById(id);
     }
 }
