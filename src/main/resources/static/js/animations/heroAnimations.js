d200a0e8bd73b98cee6470c335bf43f758a8ddfe
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-14 10:31:17 -0500

    refactor: Eliminar el atributo 'hidden' del elemento hero en home.html y optimizar la estructura del código, añadiendo identificadores para facilitar la manipulación con JavaScript

---
+++ src/main/resources/static/js/animations/heroAnimations.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () { 
+     anime({
+      targets: ".animate-badge",
+      scale: [0.95, 1],
+      opacity: [0, 1],
+      duration: 1200,
+      easing: "easeOutQuart",
+    });
+
+    // Animación del título principal
+    anime({
+      targets: "#heroTitle",
+      translateY: [20, 0],
+      opacity: [0, 1],
+      duration: 1500,
+      easing: "easeOutQuart",
+    });
+
+    // Animación de la descripción
+    anime({
+      targets: "#heroDescription",
+      translateY: [20, 0],
+      opacity: [0, 1],
+      delay: 300,
+      duration: 1500,
+      easing: "easeOutQuart",
+    });
+
+    // animacion de los botones
+    anime({
+      targets: "#heroButtons a",
+      translateX: [-20, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(200),
+      duration: 1200,
+      easing: "easeOutQuart",
+    });
+
+    // animación de las tarjetas de acceso rápido
+    anime({
+      targets: "#quickAccessCards a",
+      translateY: [30, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(300),
+      duration: 1800,
+      easing: "easeOutQuart",
+    });
+
+    // Animación de las imágenes en las tarjetas
+    anime({
+      targets: ".aspect-\\[4\\/3\\] img",
+      scale: [0.98, 1],
+      opacity: [0, 1],
+      delay: anime.stagger(200),
+      duration: 1500,
+      easing: "easeOutQuart",
+    });
+
+    // Animación del título
+    anime({
+      targets: ["#sectionTitle", "#sectionDescription"],
+      translateY: [15, 0],
+      opacity: [0, 1],
+      delay: anime.stagger(200),
+      duration: 1500,
+      easing: "easeOutQuart",
+    });
+  }); 
