007be686820a1d2198987dc21f44fa79f8650d9c
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-13 23:49:48 -0500

    refactor: Improve type safety in uploadFile method in CloudinaryService

--- src/main/java/com/api/diversity/application/service/impl/CloudinaryService.java
+++ src/main/java/com/api/diversity/application/service/impl/CloudinaryService.java
@@ -1,5 +1,5 @@
 package com.api.diversity.application.service.impl;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,19 +11,20 @@ import java.util.Map;
 
 @Service
 public class CloudinaryService {
-    
+
     @Autowired
     private Cloudinary cloudinary;
-    
+
+    @SuppressWarnings("unchecked")
     public String uploadFile(MultipartFile file) {
         try {
-            Map uploadResult = cloudinary.uploader().upload(file.getBytes(), Map.of());
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(), Map.of());
             return uploadResult.get("url").toString();
         } catch (IOException e) {
             throw new RuntimeException("Error uploading file to Cloudinary", e);
         }
     }
-    
+
     public void deleteFile(String publicId) {
         try {
             cloudinary.uploader().destroy(publicId, Map.of());
