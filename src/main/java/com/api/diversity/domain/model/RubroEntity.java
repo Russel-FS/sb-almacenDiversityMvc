769bf618ae1e00a7ed265bf4dc0ffe652f10d422
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-14 14:29:17 -0500

    s

--- src/main/java/com/api/diversity/domain/model/RubroEntity.java
+++ src/main/java/com/api/diversity/domain/model/RubroEntity.java
@@ -17,15 +17,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "Rubros")
 public class RubroEntity {
