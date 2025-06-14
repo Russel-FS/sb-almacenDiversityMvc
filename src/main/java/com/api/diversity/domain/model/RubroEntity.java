40de7b7867b8d8b7de341b46b0422901e33a0bfc
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-14 13:42:18 -0500

    feat: Actualizar RubroEntity para incluir anotaciones de creación y actualización de marcas de tiempo, y ajustar longitudes de columnas en la base de datos, mejorando la gestión de datos y la trazabilidad de cambios.

--- src/main/java/com/api/diversity/domain/model/RubroEntity.java
+++ src/main/java/com/api/diversity/domain/model/RubroEntity.java
@@ -2,6 +2,9 @@ package com.api.diversity.domain.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import com.api.diversity.domain.enums.EstadoRubro;
 
 import jakarta.persistence.Column;
@@ -29,31 +32,33 @@ public class RubroEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "IdRubro")
+    @Column(name = "ID_Rubro")
     private Long idRubro;
 
-    @Column(name = "NombreRubro", nullable = false)
+    @Column(name = "NombreRubro", nullable = false, length = 80)
     private String nombreRubro;
 
-    @Column(name = "Code", nullable = false)
+    @Column(name = "Code", nullable = false, length = 50)
     private String code;
 
-    @Column(name = "Descripcion")
+    @Column(name = "Descripcion", length = 255)
     private String descripcion;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "Estado", nullable = false)
     private EstadoRubro estado;
 
-    @Column(name = "PublicId")
+    @Column(name = "PublicId", length = 100)
     private String publicId;
 
-    @Column(name = "ImagenUrl")
+    @Column(name = "ImagenUrl", length = 255)
     private String imagenUrl;
 
-    @Column(name = "Fecha_Creacion")
+    @CreationTimestamp
+    @Column(name = "Fecha_Creacion", updatable = false)
     private LocalDateTime fechaCreacion;
 
+    @UpdateTimestamp
     @Column(name = "Fecha_Modificacion")
     private LocalDateTime fechaModificacion;
 
