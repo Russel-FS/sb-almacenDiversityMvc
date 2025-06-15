f4283d50cb455dab8280642afcb26d5cb228f0b6
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-15 01:33:29 -0500

    feat: Remove unused classes and interfaces related to DetalleEntrada and Entradas

--- src/main/java/com/api/avance3/repository/EntradasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.avance3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.avance3.model.Entradas;
-
-@Repository
-public interface EntradasRepository extends JpaRepository<Entradas, Long> {
-}
